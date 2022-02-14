9a76b538b6e0d18c1fdde902b3f822fdc4eda9b7
JavohirNew <bond020298@gmail.com>
2022-02-14 13:01:15 +0500

    3 section stayed

--- Site verstka/script.js
+++ Site verstka/script.js
@@ -4,9 +4,25 @@ const blueBtn = document.querySelector(".location__blue-btn");
 const article = document.querySelector("article");
 
 blueBtn.onclick = () => {
-    if(article.style.marginLeft ==0){
-        article.style.marginLeft = "-100%";
+    if(article.style.marginLeft == "" || article.style.marginLeft == 0){
+        article.style.animation = "none"
+        article.style.marginLeft = "-100%"
+    }else if(article.style.marginLeft == "-100%"){
+        article.style.animation = "none"
+        article.style.marginLeft = "-200%"
+    }else if(article.style.marginLeft == "-200%"){
+        article.style.animation = "none"
+        article.style.marginLeft = "";
     }else {
-        alert("xato");
+        console.log(article.style.marginLeft);
+    }
+}
+whiteBtn.onclick = () => {
+    if(article.style.marginLeft == "-100%"){
+        article.style.marginLeft = ""
+    }else if(article.style.marginLeft == "-200%"){
+        article.style.marginLeft = "-100%"
+    }else {
+        console.log(article.style.marginLeft);
     }
 }
